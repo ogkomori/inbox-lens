2bfabb4832d2f66d8f07c8e1e118d36cdf2acdef
ogkomori <majorogkomori@gmail.com>
2025-07-17 17:35:31 +0100

    Initial commit

---
+++ src/main/java/com/komori/corefocus/CorefocusApplication.java
@@ -0,0 +1,13 @@
+package com.komori.corefocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorefocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorefocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komori/corefocus/CorefocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komori.corefocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorefocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
