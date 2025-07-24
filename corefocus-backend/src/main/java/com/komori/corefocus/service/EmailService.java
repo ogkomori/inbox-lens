fc44f0087104bccd8198719bc8b44b1456cc1606
ogkomori <majorogkomori@gmail.com>
2025-07-24 20:40:03 +0100

    Added mail logic

--- corefocus-backend/src/main/java/com/komori/corefocus/service/EmailService.java
+++ corefocus-backend/src/main/java/com/komori/corefocus/service/EmailService.java
@@ -1,32 +1,21 @@
 package com.komori.corefocus.service;
 
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.gmail.Gmail;
-import com.komori.corefocus.entity.UserEntity;
-import com.komori.corefocus.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-    private final UserRepository userRepository;
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+    private JavaMailSender javaMailSender;
 
-    private Gmail loadTokens(@CurrentSecurityContext(expression = "authentication?.name") String sub) {
-        UserEntity currentUser = userRepository.findBySub(sub)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Credential.AccessMethod accessMethod = BearerToken.authorizationHeaderAccessMethod();
-        Credential credential = new Credential(accessMethod);
-        credential.setAccessToken(currentUser.getAccessToken());
-        credential.setRefreshToken(currentUser.getRefreshToken());
-        credential.setExpirationTimeMilliseconds(currentUser.getAccessTokenExpiresAt().toEpochMilli());
-
-        return new Gmail(new NetHttpTransport(), GsonFactory.getDefaultInstance(), credential);
+    public void sendSummaryEmail(String toEmail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        // add Mail details
+        javaMailSender.send(message);
     }
 }
