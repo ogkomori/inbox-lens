9344dbf2920d228ecffaaf44cb13a479db962fc5
ogkomori <majorogkomori@gmail.com>
2025-07-24 20:44:33 +0100

    Moved jwtUtil.java

---
+++ corefocus-backend/src/main/java/com/komori/corefocus/security/JwtUtil.java
@@ -0,0 +1,85 @@
+package com.komori.corefocus.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret.key}")
+    private String STORED_SECRET_KEY;
+
+    public String generateAccessToken(String sub) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims(claims)
+                .subject(sub)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) // 15 min expiration
+                .signWith(Keys.hmacShaKeyFor(STORED_SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    public String generateRefreshToken(String sub) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims(claims)
+                .subject(sub)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 14)) // 14 day expiration
+                .signWith(Keys.hmacShaKeyFor(STORED_SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    public String extractRefreshTokenFromCookie(HttpServletRequest request) {
+        String token = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refresh")) {
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        return token;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(STORED_SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractSubFromToken(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        Claims claims = extractAllClaims(token);
+        Date expiration = claims.getExpiration();
+        return expiration.before(new Date());
+    }
+
+    public Boolean validateAccessToken(String token, String sub) {
+        final String tokenSub = extractSubFromToken(token);
+        return sub.equals(tokenSub) && !isTokenExpired(token);
+    }
+
+    public Boolean validateRefreshToken(String token, String sub) {
+        final String receivedEmail = extractSubFromToken(token);
+        return sub.equals(receivedEmail) && !isTokenExpired(token);
+    }
+}
