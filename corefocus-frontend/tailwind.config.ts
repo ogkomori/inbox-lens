3ec07a9dc7fdea1df55c81364eb77d83158d7345
ogkomori <majorogkomori@gmail.com>
2025-07-29 18:32:56 +0100

    Implemented Poppins font

--- corefocus-frontend/tailwind.config.ts
+++ corefocus-frontend/tailwind.config.ts
@@ -17,7 +17,10 @@ export default {
 				'2xl': '1400px'
 			}
 		},
-		extend: {
+	   extend: {
+   fontFamily: {
+  sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+		   },
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
