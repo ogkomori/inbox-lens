3ae4b1117efec786dffcde7930e352929102cce7
ogkomori <majorogkomori@gmail.com>
2025-08-19 01:43:01 +0100

    Created summaryPrompt bean

--- corefocus-backend/src/main/java/com/komori/corefocus/config/OpenAIConfig.java
+++ corefocus-backend/src/main/java/com/komori/corefocus/config/OpenAIConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 @Configuration
 public class OpenAIConfig {
     @Value("${open.api.key}")
@@ -17,5 +21,19 @@ public class OpenAIConfig {
                 .defaultHeader("Content-Type", "application/json")
                 .defaultHeader("Authorization", "Bearer " + OPEN_API_KEY)
                 .build();
+    }
+
+    @Bean
+    public String summaryPrompt() {
+        try(InputStream inputStream = OpenAIConfig.class
+                .getClassLoader()
+                .getResourceAsStream("prompts/summary_prompt.txt")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("SummaryPrompt file not found");
+            }
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
