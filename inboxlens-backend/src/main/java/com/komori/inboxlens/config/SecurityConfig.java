9ede2305704b74265925dbfd999c5e32b36bdb72
ogkomori <majorogkomori@gmail.com>
2025-08-31 10:41:07 +0100

    Made /debug public

--- inboxlens-backend/src/main/java/com/komori/inboxlens/config/SecurityConfig.java
+++ inboxlens-backend/src/main/java/com/komori/inboxlens/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/api/contact/**", "/oauth2/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/debug", "/api/auth/**", "/api/contact/**", "/oauth2/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(login -> login.successHandler(successHandler))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
